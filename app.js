9e8c6d5e35781595ef462b65385880803c9e2fd3
Martin-Contreras <martincb858@gmail.com>
2025-08-16 12:06:28 -0600

    modificaciones finales

--- app.js
+++ app.js
@@ -9,23 +9,57 @@ function agregarAmigo() {
     }else{
         if (amigos.includes(nombreAmigo)){
             alert("El amigo ya está en la lista.");
+            limpiarInput();
             return;
         }else{
-            amigos.push(nombreAmigo);
-            mostrarAmigos(nombreAmigo);
-            limpiarInput();
+            agregarLista(nombreAmigo);
             return;
         }
     }  
 }
 
-function limpiarInput(){
+function limpiar(){
     document.getElementById("amigo").value = "";
+    document.getElementById("resultado").innerHTML = "";
+    return;
 }
 
-function mostrarAmigos(nombre) {
+function agregarLista(nombreAmigo){
+    amigos.push(nombreAmigo);
+    limpiar();
+    mostrarAmigos(nombreAmigo, "listaAmigos");
+    return;
+}
+
+function limpiarArray(){
+    amigos =[];
+    document.getElementById("listaAmigos").innerHTML = "";
+    return;
+}
+
+
+function mostrarAmigos(nombre, listaDestino) {
     let nuevoLi = document.createElement("li");
-    let lista = document.getElementById("listaAmigos");
+    let lista = document.getElementById(listaDestino);
     nuevoLi.textContent = nombre;
     lista.appendChild(nuevoLi);
+    return;
+}
+
+function generarNumero(){
+    let numero = Math.floor(Math.random() * amigos.length)
+    return numero;
+}
+
+function sortearAmigo(){
+    if (amigos.length > 0) {
+        let numero = generarNumero();
+        mostrarAmigos(amigos[numero], "resultado");
+        limpiarArray();
+        return;
+        
+    }else{
+        alert("Por favor, agrega amigos a la lista antes de sortear.");
+        return;
+    }
 }
